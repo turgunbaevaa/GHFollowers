843e6bdb63a32467e8a7249b2fc20a25f9a1f1b2
turgunbaevaa <nyc.aruu@gmail.com>
2024-02-24 12:03:43 +0100

    Added UITabBarController

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Aruuke Turgunbaeva on 24/2/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Aruuke Turgunbaeva on 24/2/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
